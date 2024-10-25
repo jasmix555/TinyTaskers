d23dcf166286aa46446d642b0234eb94119dbf85
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-10-25 10:04:50 +0900

    chore: reset to next.js

--- src/api/supabase/index.ts
+++
@@ -1,7 +0,0 @@
-// src/api/supabase/index.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
