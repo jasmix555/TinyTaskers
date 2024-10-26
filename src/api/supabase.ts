4542a4096052121ab5f369f0cbaa2d250147480f
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2024-10-26 13:12:36 +0900

    chore: remove supabase

--- src/api/supabase.ts
+++
@@ -1,7 +0,0 @@
-// src/api/supabaseClient.ts
-import {createClient} from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseApiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseApiKey);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,8 @@
-// src/app/page.tsx
 "use client";
-import {useEffect, useState} from "react";
-import {useRouter} from "next/navigation";
-import {User} from "@supabase/supabase-js"; // Supabase User type
-
-import {supabase} from "@/api/supabase";
-
 export default function Home() {
-  const [user, setUser] = useState<User | null>(null); // User type from Supabase
-  const router = useRouter();
-
-  // Check if the user is logged in
-  useEffect(() => {
-    const checkUser = async (): Promise<void> => {
-      const {
-        data: {session},
-      } = await supabase.auth.getSession();
-
-      if (session) {
-        setUser(session.user);
-      } else {
-        router.push("/welcome"); // Redirect to Welcome page if not logged in
-      }
-    };
-
-    checkUser();
-  }, [router]);
-
-  const handleLogout = async (): Promise<void> => {
-    await supabase.auth.signOut();
-    router.push("/welcome");
-  };
-
-  if (!user) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="">
-      <h1>Hello, {user.email}!</h1>
-      <button className="mt-4 rounded bg-red-500 px-8 py-2 text-white" onClick={handleLogout}>
-        Logout
-      </button>
+      <h1>Hello World</h1>
     </div>
   );
 }
