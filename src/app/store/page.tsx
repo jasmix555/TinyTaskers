729f1982d8605691238bed89962d311c99867741
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2025-01-16 12:25:32 +0900

    chore: change store design

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -3,7 +3,8 @@
 import React, {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import {collection, getDocs, doc, onSnapshot, runTransaction} from "firebase/firestore";
-import {FaCaretLeft, FaSackDollar} from "react-icons/fa6";
+import {FaCaretDown, FaCaretLeft, FaSackDollar} from "react-icons/fa6";
+import Image from "next/image";
 
 import {db} from "@/api/firebase";
 import {useAuth} from "@/hooks";
@@ -173,82 +174,114 @@ export default function StorePage() {
   if (!user) return <Loading />;
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="mb-4 text-center text-2xl font-bold">Store</h1>
+    <div className="h-screen">
       {error && <p className="text-red-500">{error}</p>}
-
-      {/* Child Selection Dropdown */}
-      <div className="mb-4">
-        <label className="block font-bold" htmlFor="childSelect">
-          Select Child:
-        </label>
-        <select
-          className="mt-2 w-full rounded-lg border border-gray-300 p-2"
-          id="childSelect"
-          value={selectedChild?.id || ""}
-          onChange={(e) => {
-            const selectedChild = children.find((child) => child.id === e.target.value);
-
-            selectChild(selectedChild!);
-          }}
-        >
-          {children.map((child) => (
-            <option key={child.id} value={child.id}>
-              {child.name}
-            </option>
-          ))}
-        </select>
-      </div>
 
       {/* Points Summary */}
       {selectedChild && (
-        <div className="mb-4 flex justify-between rounded-lg bg-gray-100 p-4">
-          <span className="text-lg font-bold">{selectedChild.name}&apos;s Points</span>
-          <span className="flex items-center gap-1 text-xl font-bold text-orange-500">
-            <FaSackDollar /> {childPoints ?? 0}
-          </span>
+        <div className="flex w-full items-center justify-between bg-orange-300 p-4 text-white">
+          <div className="relative flex items-center gap-3 rounded-lg">
+            <div className="h-14 w-14 overflow-hidden rounded-full">
+              <Image
+                priority
+                alt={selectedChild.name}
+                className="rounded-full"
+                height={200}
+                src={selectedChild.picture || "/default-child.png"}
+                width={200}
+              />
+            </div>
+
+            {/* Child Name with Dropdown */}
+            <div className="relative">
+              <button
+                className="flex items-center gap-2 rounded-xl bg-gray-600/25 px-4 py-2 text-xl"
+                onClick={() => setIsModalOpen((prev) => !prev)} // Toggle dropdown visibility
+              >
+                {selectedChild.name}
+                <FaCaretDown />
+              </button>
+
+              {/* Dropdown Menu */}
+              {isModalOpen && (
+                <div className="absolute left-0 top-12 z-10 w-48 rounded-lg border bg-white shadow-lg">
+                  <ul>
+                    {children.map((child) => (
+                      <li
+                        key={child.id}
+                        className={`cursor-pointer p-2 text-black hover:bg-gray-100 ${
+                          child.id === selectedChild.id ? "font-bold" : ""
+                        }`}
+                        onClick={() => {
+                          selectChild(child);
+                          setIsModalOpen(false); // Close dropdown after selection
+                        }}
+                      >
+                        {child.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+          <p className="flex items-center gap-2 font-bold sm:text-xl md:text-4xl">
+            <span className="font-normal sm:text-lg md:text-3xl">
+              <FaSackDollar />
+            </span>
+            {childPoints}
+          </p>
         </div>
       )}
 
       {/* Rewards List */}
-      {filteredRewards.length === 0 ? (
-        <p className="text-center">No rewards available.</p>
-      ) : (
-        <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {filteredRewards.map((reward) => {
-            const Icon = TaskIcons.find((icon) => icon.id === reward.icon)?.icon || FaSackDollar;
-            const ownedCount = rewardCounts[reward.id] || 0;
+      <div
+        className="no-scrollbar m-4 overflow-y-scroll rounded-lg bg-orange-200 p-4 shadow-md"
+        style={{
+          height: "calc(100vh - 7.8rem)",
+        }}
+      >
+        {filteredRewards.length === 0 ? (
+          <p className="text-center">No rewards available.</p>
+        ) : (
+          <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            {filteredRewards.map((reward) => {
+              const Icon = TaskIcons.find((icon) => icon.id === reward.icon)?.icon || FaSackDollar;
+              const ownedCount = rewardCounts[reward.id] || 0;
 
-            return (
-              <li
-                key={reward.id}
-                className="flex flex-col items-center justify-between rounded-lg border bg-white p-4 shadow-md"
-              >
-                <div className="mb-2 rounded-lg border p-4 text-5xl shadow-sm">
-                  <Icon />
-                </div>
-                <div className="flex flex-col gap-2 text-center">
-                  <p className="text-2xl font-bold">{reward.title}</p>
-                  <p className="flex items-center justify-center gap-1 text-xl text-gray-600">
-                    <FaSackDollar /> {reward.pointsRequired}
-                  </p>
-                  <p className="text-md text-gray-500">Owned: x{ownedCount}</p>
-                </div>
-                <button
-                  className="mt-4 rounded-lg bg-orange-300 px-4 py-2 text-white hover:bg-orange-400 disabled:bg-gray-300"
-                  disabled={!selectedChild || (childPoints ?? 0) < reward.pointsRequired}
-                  onClick={() => {
-                    setSelectedReward(reward);
-                    setIsModalOpen(true);
-                  }}
+              return (
+                <li
+                  key={reward.id}
+                  className="flex flex-col items-center justify-between rounded-lg border bg-white p-4 shadow-md"
                 >
-                  {(childPoints ?? 0) >= reward.pointsRequired ? "Purchase" : "Insufficient Points"}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      )}
+                  <div className="mb-2 rounded-lg border p-4 text-5xl shadow-sm">
+                    <Icon />
+                  </div>
+                  <div className="flex flex-col gap-2 text-center">
+                    <p className="text-2xl font-bold">{reward.title}</p>
+                    <p className="flex items-center justify-center gap-1 text-xl text-gray-600">
+                      <FaSackDollar /> {reward.pointsRequired}
+                    </p>
+                    <p className="text-md text-gray-500">Owned: x{ownedCount}</p>
+                  </div>
+                  <button
+                    className="mt-4 rounded-lg bg-orange-300 px-4 py-2 text-white hover:bg-orange-400 disabled:bg-gray-300"
+                    disabled={!selectedChild || (childPoints ?? 0) < reward.pointsRequired}
+                    onClick={() => {
+                      setSelectedReward(reward);
+                      setIsModalOpen(true);
+                    }}
+                  >
+                    {(childPoints ?? 0) >= reward.pointsRequired
+                      ? "Purchase"
+                      : "Insufficient Points"}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
 
       {/* Modal for Purchase Confirmation */}
       {isModalOpen && selectedReward && (
@@ -287,9 +320,9 @@ export default function StorePage() {
         </div>
       )}
 
-      <div className="absolute bottom-9 left-14 z-0 rounded-lg">
+      <div className="absolute bottom-12 left-10 z-0 rounded-lg">
         <button
-          className="flex items-center gap-2 rounded-lg border bg-gray-300 bg-white p-4 px-4 py-2 shadow-md hover:bg-gray-100"
+          className="flex items-center gap-2 rounded-lg border bg-white p-4 px-4 py-2 shadow-md hover:bg-gray-100"
           onClick={() => {
             if (selectedChild && routerReady) {
               router.push(`/child-dashboard/${selectedChild.id}`);
